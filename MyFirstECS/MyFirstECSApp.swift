51531a0ce936eca2bcea92670df89c75915aebc4
Danilo Campos <mail@danilocampos.com>
2024-04-01 10:08:15 -0400

    Initial Commit

---
+++ MyFirstECS/MyFirstECSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstECSApp.swift
+//  MyFirstECS
+//
+//  Created by Danilo Campos on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstECSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
